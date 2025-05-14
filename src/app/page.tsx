4ea15c762d00c8e5475ae6095816d37a5871a728
Diego Hernandez <diego@udundi.com>
2025-05-14 11:10:42 -0600

    added ad container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HpEvents from "./components/HpEvents";
 import HpAppointmentList from "./components/HpAppointmentList";
+import { AdContainerFull } from "@components/layouts/AdContainer"
 
 export default function Home() {
   return (
     <main className="flex-grow bg-white sm:p-12 p-6 md:px-32">
       <HpEvents />
       <HpAppointmentList />
+      <AdContainerFull />
     </main>
   );
 }
