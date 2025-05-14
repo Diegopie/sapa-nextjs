21a95e7038139b4ca1e67f215fa1bb81586362ff
Diego <diego@udundi.com>
2025-05-14 16:33:39 -0600

    updated for mobile, updated a tags for ally and now link to maps

--- src/components/PharmaciesList.tsx
+++ src/components/PharmaciesList.tsx
@@ -9,24 +9,31 @@ export default function PharmaciesList({ pharmacies }: PharmaciesListProps) {
   return (
     <div className="grid grid-cols-1 gap-6">
       {pharmacies.map((pharmacy) => (
-        <div key={pharmacy.id} className="pharmacy-card mt-2 w-full">
-          <h3 className="font-bold text-ug-green-two">{pharmacy.title}</h3>            <div className="mt-2">
-            {/* Locations */}
-            {pharmacy.locations.map((location, index) => (
-              <div key={index} className="grid grid-cols-1 md:grid-cols-3 gap-2 mb-3 text-sm">
-                <div className="col-span-1">
-                  <p className="text-gray-700">{location.address}</p>
+        <div key={pharmacy.id} className="px-18 md:px-0 mt-2 w-full">
+          <h3 className="font-bold text-ug-green-two">{pharmacy.title}</h3>            <div className="mt-2">            {/* Locations */}
+                {pharmacy.locations.map((location, index) => (              <div key={index} className="flex flex-col md:flex-row justify-between mb-3 text-sm">
+                <div className="md:w-1/3 mb-2 md:mb-0">                  <a 
+                    href={`https://maps.google.com/?q=${encodeURIComponent(location.address)}`}
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className="text-gray-700 hover:text-green-700 hover:underline focus:text-green-700 focus:underline"
+                  >
+                    {location.address}
+                  </a>
+                </div>                <div className="md:w-1/3 text-left md:text-center mb-2 md:mb-0">
+                  <a 
+                    className="text-gray-600 hover:underline focus:underline hover:text-green-700 focus:text-green-700" 
+                    href={`tel:1${location.phone}`}
+                  >
+                    {location.phone}
+                  </a>
                 </div>
-                <div className="col-span-1 text-center">
-                  <a className="text-gray-600" href={`tel:1${location.phone}`}>{location.phone}</a>
-                </div>
-                <div className="col-span-1 text-right">
-                  {location.website && (
-                    <a 
+                <div className="md:w-1/3 text-left md:text-right">
+                  {location.website && (                    <a 
                       href={`https://${location.website}`}
                       target="_blank" 
                       rel="noopener noreferrer"
-                      className="text-green-700 hover:underline"
+                      className="text-green-700 hover:underline focus:underline"
                     >
                       {location.website}
                     </a>
